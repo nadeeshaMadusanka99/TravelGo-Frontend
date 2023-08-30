3628277e858dd01b27317395f990571fc52254b0
movinsilva <movinpinsara@gmail.com>
2023-08-30 20:24:25 +0530

    project setup. routes redux added

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import store from './store'
+import { Provider } from 'react-redux'
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.scss'
+import HomeScreen from './screens/HomeScreen.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
+);
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+
+const HomeScreen = () => {
+  return (
+    <HeroSection />
+  )
+}
+
+export default HomeScreen
