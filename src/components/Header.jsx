e7c62b196b494e09811636859080575186f8ab3c
Nadeesha Madusanka <Nadeeshamadusanka44@gmail.com>
2023-09-01 17:40:58 +0530

    added the logo to the navbar and made it clickable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,28 +2,40 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 import './Header.scss'
+// Import the logo image
+import logo from '../assets/LogoBlue.png';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand>Travel Go</Navbar.Brand>
-          </LinkContainer>
+          <Navbar.Brand href="./">
+            <img
+              alt=""
+              src={logo}
+              className="logo"
+            />{' '}
+            Travel Go
+          </Navbar.Brand>
+
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
+
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
+
               <LinkContainer to='/login'>
                 <Nav.Link>
                   <FaSignInAlt /> Sign In
                 </Nav.Link>
               </LinkContainer>
+
               <LinkContainer to='/register'>
                 <Nav.Link>
                   <FaSignOutAlt /> Sign Up
                 </Nav.Link>
               </LinkContainer>
+
             </Nav>
           </Navbar.Collapse>
         </Container>
