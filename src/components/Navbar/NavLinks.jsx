9c1ab41f2d22511af5b788a3a949efe5cbd4d19a
shehan2000 <maliths.20@cse.mrt.lk>
2023-09-15 10:28:53 +0530

    connected with the server
    server connection established and updated navigation

--- src/components/Navbar/NavLinks.jsx
+++ src/components/Navbar/NavLinks.jsx
@@ -5,7 +5,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 const NavLinks = () => {
   const links = [
-    { to: './', text: 'Home' },
+    { to: './booking', text: 'Home' },
     { to: '/Services', text: 'Services' },
     { to: '/Bookings', text: 'Bookings' },
     { to: '/Terms', text: 'Terms & Conditions' },
