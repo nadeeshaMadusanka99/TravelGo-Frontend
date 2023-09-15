98be36566b25041046631ad9f78722035b436c27
shehan2000 <88200031+shehan2000@users.noreply.github.com>
2023-09-15 10:36:45 +0530

    Update MapContainer.jsx

--- src/Screens/Booking/MapContainer.jsx
+++ src/Screens/Booking/MapContainer.jsx
@@ -29,9 +29,11 @@ const MapContainer = ({ trainName, trainType, trainNo, schedule, classes }) => {
               <p className="gray-paragraph">Train No. {trainNo}</p>
               <h6 className="body-paragraph">Daily</h6>
               <p className="gray-paragraph">Classes: {classes}</p>
+              <LinkContainer to="/schedule">
               <Button variant="primary" className="btn-book">
                 Book Tickets
               </Button>
+              </LinkContainer>
             </Col>
 
             <Col>
