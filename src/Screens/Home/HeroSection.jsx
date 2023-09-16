230233aadbed9dc54308d4a304594078fc3f7d8e
shehan2000 <maliths.20@cse.mrt.lk>
2023-09-16 22:31:37 +0530

    jsx updated

--- src/Screens/Home/HeroSection.jsx
+++ src/Screens/Home/HeroSection.jsx
@@ -1,6 +1,7 @@
 import { Container, Button } from 'react-bootstrap';
 import './HeroSection.scss';
 import { useGetStationsQuery } from '../../slices/trainApiSlice';
+import { LinkContainer } from 'react-router-bootstrap';
 
 
 const HeroSection = () => {
@@ -21,17 +22,41 @@ const HeroSection = () => {
       
         <div className="search-form">
           <div className="glass-container-extend">
+            <div className='dropdown-class'>
+            <label className='dropdown-label'>From</label>
             <select>
               {(data!=undefined) ? data.map((station) => (
                 <option>{station.StationName}</option>
               )) : <><option>Loading</option></>}
            
             </select>
-            <input type="text" placeholder="From" />
-            <input type="text" placeholder="To" />
-            <input type="date" placeholder="Date" />
+            </div>
             
+
+            <div className='dropdown-class'>
+            <label className='dropdown-label'>To</label>
+            <select>
+              {(data!=undefined) ? data.map((station) => (
+                <option>{station.StationName}</option>
+              )) : <><option>Loading</option></>}
+           
+            </select>
+            </div>
+
+            <div className='dropdown-class'>
+            <label className='dropdown-label'>Date</label>
+            <input type="date" placeholder="Date" />
+
+
+            </div>
+
+            
+            
+            <div className='search-btn'>
+            <LinkContainer to="/booking">
             <Button variant="primary" className='button-extend'>Search</Button>
+            </LinkContainer>
+            </div>
           </div>
         </div>
       </Container>
