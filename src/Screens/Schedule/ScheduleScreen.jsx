5b3e141427460d97a001f2943f48f3648d445d8b
shehan2000 <maliths.20@cse.mrt.lk>
2023-09-17 23:47:50 +0530

    responsive schedule component
    updated the new schedule component

--- src/Screens/Schedule/ScheduleScreen.jsx
+++ src/Screens/Schedule/ScheduleScreen.jsx
@@ -1,10 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import Schedule from './Schedule'
+import NewSchedule from './NewSchedule'
 
 const ScheduleScreen = () => {
   return (
-    <Schedule />
+    <NewSchedule />
   )
 }
 
