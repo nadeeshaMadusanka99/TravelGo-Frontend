de6762544af504d80ee5b098a2d24bce431ff2ec
shehan2000 <maliths.20@cse.mrt.lk>
2023-09-29 13:03:49 +0530

    Newschedule
    responsive design completed,
    scss adding finished

--- src/Screens/Schedule/NewSchedule.jsx
+++ src/Screens/Schedule/NewSchedule.jsx
@@ -6,7 +6,7 @@ import { LinkContainer } from "react-router-bootstrap";
 
 const ClassDetails = ({ seatClass, price }) => {
   const [counter, setCounter] = useState(0);
-
+  let available=25;
   const increment = () => {
     setCounter(counter + 1);
   };
@@ -19,20 +19,28 @@ const ClassDetails = ({ seatClass, price }) => {
   return(
     <Container fluid="true" className="count-container">
           <Row>
-            <Col md="3" sm="6" xs='6'>
+            <Col md="4" sm="6" xs='6' className="seat-class">
                   <h2>{seatClass}</h2>
             </Col>
             <Col md="2" sm="6" xs='6'>
-                  <Row>60 seats</Row>
-                  <Row>{price} LKR</Row>
+                  <Row className="seat-cnt">60 seats</Row>
+                  <Row className="tkt-price">{price} LKR</Row>
             </Col>
-            <Col md="3" sm="6" xs='6'>
-                  <Row>Available:25</Row>
-                  <Row>Booked:35</Row>
+            <Col md="4" sm="6" xs='6'>
+                  <Row className="seat-grey">
+                  <Col xs={6}>
+                     <p>Available:</p>
+                    </Col>
+                <Col xs={6}>
+                <p className="avai-para">{available}</p>
+                </Col></Row>
+                  <Row className="seat-grey">
+                  <Col xs={6} >Booked:</Col>
+                  <Col xs={6} className="booked-para">35</Col></Row>
             </Col>
-            <Col md="4" sm="6" xs='6' className="d-flex justify-content-center align-items-center">
+            <Col md="2" sm="6" xs='6' className="d-flex justify-content-center align-items-center">
               <Col md="6" sm="6" xs='6'>
-              <span style={{ marginTop: '-30px' }}>{counter}</span>
+              <span className="count-span" style={{ marginTop: '-30px' }}>{counter}</span>
               </Col>
               <Col md="6" sm="6" xs='6'>
                 <Row>
@@ -168,7 +176,7 @@ const NewSchedule = () => {
         </Row>
       </Container>
 
-      <Container fluid="true" className="heading-container">
+      <Container fluid="true" className="headingContainer">
          <Row>
           <Col md="9" sm="12">
             <h1>How many seats do you want to book?</h1>        
@@ -186,20 +194,27 @@ const NewSchedule = () => {
          <ClassDetails seatClass="First-Class" price="1850" />
          <ClassDetails seatClass="Second Class" price="950" />
          <ClassDetails seatClass="Third Class" price="550" />
-        <Container className="button-container" md='12'>
-          <Row className="jus" style={{ margin: '20px'}}>
-                <Col md="9" sm="12" className='d-flex align-items-start justify-content-start'>
-                    <Row><p className='ticket-count'>2 First Class</p><p className='seat-para'> Seats selected</p></Row>
-                    <Row>
-                    <p className='ticket-price'>3100 LKR</p>
-                    <p className='cost-para'> Costs can vary due to offers and other promotional matters</p>
-                    </Row>
+        <Container className="buttonContainer" >
+          <Row >
+            <Col md="9" sm="12" >
+              <Row>
+                <Col xs={6} sm={4}  xl={2}><p className='ticket-count'>2 First Class</p></Col>
+                <Col xs={6} sm={4} xl={3}><p className='seat-para'> Seats selected</p></Col>
+              </Row>
+              <Row>
+                <Col xs={12} sm={4} xl={2}>
+                      <p className='ticket-price'>3100 LKR</p>
                 </Col>
-                <Col md="3" sm="12" className='d-flex align-items-center justify-content-end'>
+                <Col xs={12} sm={8}  >
+                      <p className='cost-para'>Costs can vary due to offers and other promotional matters</p>
+                </Col>
+              </Row>
+            </Col>
+            <Col md="3" sm="12" className='book-btnc'>
                   <LinkContainer to="/seatview">
-                    <Button variant="primary" className="btn-book">Continue</Button>
+                    <Button variant="primary" className="btnBook">Continue</Button>
                   </LinkContainer>
-                </Col>
+            </Col>
           </Row>
           </Container>
     </Container>
