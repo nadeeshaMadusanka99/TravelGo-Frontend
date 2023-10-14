73639bd4f8aa0aeb5f9dd475a405e7238b6e7a2c
Movin Silva <movinpinsara@gmail.com>
2023-10-14 11:40:12 +0530

    bugs

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,7 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const LocalStorage = require('node-localstorage').LocalStorage;
-const localStorage = new LocalStorage('./scratch');
+
 
 const initialState = {
   userInfo: localStorage.getItem('userInfo')
